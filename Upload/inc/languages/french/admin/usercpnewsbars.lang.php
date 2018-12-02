472ce879cd59b8bd7101b0f78a4dfd0705ca6f91
vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-01 18:59:59 -0800

    Update usercpnewsbars.lang.php

--- Upload/inc/languages/french/admin/usercpnewsbars.lang.php
+++ Upload/inc/languages/french/admin/usercpnewsbars.lang.php
@@ -15,7 +15,7 @@
 // plugin_info
 
 $l['usercpnewsbars_name'] = 'UserCP Newsbars';
-$l['usercpnewsbars_desc'] = 'Ajoute des newsbars à la page usercp de votre forum.';
+$l['usercpnewsbars_desc'] = 'Ajoute des newsbars a la page usercp de votre forum.';
 $l['usercpnewsbars_web'] = 'http://community.mybb.com/user-6029.html';
 $l['usercpnewsbars_auth'] = 'Vintagedaddyo';
 $l['usercpnewsbars_authsite'] = 'http://community.mybb.com/user-6029.html';
@@ -25,12 +25,12 @@ $l['usercpnewsbars_compat'] = '18*';
 // Groupe de paramètres
 
 $l['usercpnewsbars_title_setting_group'] = 'Barres de discussion UserCP';
-$l['usercpnewsbars_description_setting_group'] = 'Paramètres du plugin UserCP Newsbars.';
+$l['usercpnewsbars_description_setting_group'] = 'Parametres du plugin UserCP Newsbars.';
 
 // Réglage 1
 
 $l['usercpnewsbars_title_setting_1'] = 'Voulez-vous activer UserCP Newsbars Plugin sur UserCP?';
-$l['usercpnewsbars_description_setting_1'] = 'Si vous définissez cette option sur yes, ce plugin sera actif sur votre index.';
+$l['usercpnewsbars_description_setting_1'] = 'Si vous definissez cette option sur yes, ce plugin sera actif sur votre index.';
 
 // Réglage 2
 
@@ -40,7 +40,7 @@ $l['usercpnewsbars_description_setting_2'] = 'Entrez le texte que vous voulez af
 // Réglage 3
 
 $l['usercpnewsbars_title_setting_3'] = 'Voulez-vous activer UserCP Newsbars Alert?';
-$l['usercpnewsbars_description_setting_3'] = 'Si vous définissez cette option sur yes, Alert sera actif sur votre usercp.';
+$l['usercpnewsbars_description_setting_3'] = 'Si vous definissez cette option sur yes, Alert sera actif sur votre usercp.';
 
 // Réglage 4
 
@@ -50,7 +50,7 @@ $l['usercpnewsbars_description_setting_4'] = 'Entrez le texte que vous voulez af
 // Réglage 5
 
 $l['usercpnewsbars_title_setting_5'] = 'Voulez-vous activer UserCP Newsbars Notice 1?';
-$l['usercpnewsbars_description_setting_5'] = 'Si vous définissez cette option sur oui, Notification 1 sera active sur votre usercp.';
+$l['usercpnewsbars_description_setting_5'] = 'Si vous definissez cette option sur oui, Notification 1 sera active sur votre usercp.';
 
 // Réglage 6
 
@@ -60,21 +60,21 @@ $l['usercpnewsbars_description_setting_6'] = 'Entrez le texte que vous voulez af
 // Réglage 7
 
 $l['usercpnewsbars_title_setting_7'] = 'Voulez-vous activer UserCP Newsbars Notice 2?';
-$l['usercpnewsbars_description_setting_7'] = 'Si vous définissez cette option sur oui, Notification 2 sera active sur votre usercp.';
+$l['usercpnewsbars_description_setting_7'] = 'Si vous definissez cette option sur oui, Notification 2 sera active sur votre usercp.';
 
 // Réglage 8
 
 $l['usercpnewsbars_title_setting_8'] = 'CSS pour l alerte Newsbars';
-$l['usercpnewsbars_description_setting_8'] = 'Ceci contrôle le style de l alerte.';
+$l['usercpnewsbars_description_setting_8'] = 'Ceci controle le style de l alerte.';
 
 // Réglage 9
 
 $l['usercpnewsbars_title_setting_9'] = 'CSS pour Newsbars Notice 1';
-$l['usercpnewsbars_description_setting_9'] = 'Ceci contrôle le style de la notice1.';
+$l['usercpnewsbars_description_setting_9'] = 'Ceci controle le style de la notice1.';
 
 // Réglage 10
 
 $l['usercpnewsbars_title_setting_10'] = 'CSS pour Newsbars Notice 2';
-$l['usercpnewsbars_description_setting_10'] = 'Ceci contrôle le style de notice2.';
+$l['usercpnewsbars_description_setting_10'] = 'Ceci controle le style de notice2.';
 
 ?>
